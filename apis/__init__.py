1d639a61f8596ea33fbaa66b00c1ca913083c10c
RocketLL <luc4sl33@gmail.com>
2019-03-24 20:16:40 +0900

    changed into chatbot

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,6 +2,6 @@ from flask_restplus import Api
 
 from .meal import api as meal
 
-api = Api(title="HAS Api", version="0.1", description="Hana Academy Seoul API")
+api = Api(title="HAS Chatbot", version="0.1", description="Hana Academy Seoul Chatbot")
 
-api.add_namespace(meal, path="/has-api/meal")
+api.add_namespace(meal, path="/has-chatbot/meal")

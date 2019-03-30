d1849f0e6ced4ae8cde72b6890d0ed74eecc491c
RocketLL <luc4sl33@gmail.com>
2019-03-31 02:33:35 +0900

    using shelve

--- apis/meal.py
+++ apis/meal.py
@@ -1,8 +1,11 @@
 from flask import request, jsonify
 from flask_restplus import Namespace, Resource
-from core.meal import *
+from core.meal import Meal
+import shelve
 
 api = Namespace("meal", description="Get school meals")
+Meals = Meal()
+f = shelve.open("meal.db")
 
 
 @api.route("/")
@@ -10,18 +13,24 @@ class Meal(Resource):
     def post(self):
         ymd = request.get_json()["action"]["params"]
         y, m, d = ymd["y"], ymd["m"], ymd["d"]
-        meal = get_meal(y,m,d)
+        breakfast, lunch, dinner, snack = (
+            f["breakfast"],
+            f["lunch"],
+            f["dinner"],
+            f["snack"],
+        )
+
         return jsonify(
             version="2.0",
             template={
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": f"오늘의 급식입니다.\n아침: {meal[0]}\n점심: {meal[1]}\n저녁: {meal[2]}\n 간식: {meal[3]}"
+                            "text": f"오늘의 급식입니다.\n아침: {breakfast}\n점심: {lunch}\n저녁: {dinner}\n 간식: {snack}"
                         }
                     }
                 ]
-            }
+            },
         )
 
 
--- core/meal.py
+++ core/meal.py
@@ -1,42 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import shelve
 
 now = datetime.now()
+f = shelve.open("meal.db")
+
+class Meal:
+    def __init__(self):
+        self.y = now.year
+        self.m = now.month
+        self.d = now.day
+        self.meal = self.get_meal(self.y, self.m, self.d)
+        self.update_meal()
+        self.write_meal()
+
+    def get_raw(self, y, m, d):
+        """
+        Gets raw meal HTML from http://hana.hs.kr/life/meal.asp
+        """
+        date = {"yy": y, "mm": m, "dd": d}
+        r = requests.get("http://hana.hs.kr/life/meal.asp", params=date)
+        r.encoding = "utf-8"
+        return r.text
+
+    def parse_meal(self, raw):
+        """
+        Parses raw meal HTML into a list
+        """
+        meals = []
+        soup = BeautifulSoup(raw, "lxml")
+        for e in soup.select_one(".today_meal").find_all("td"):
+            meals.append(e.text)
+        return meals
+
+    def get_meal(self, y, m, d):
+        """
+        Uses get_raw() and parse_meal() to return meals.
+        Defaults to current year, month, and day if not specified.
+        """
+        if y == None:
+            y = now.year
+        if m == None:
+            m = now.month
+        if d == None:
+            d = now.day
+
+        raw = self.get_raw(y, m, d)
+        return self.parse_meal(raw)
 
 
-def get_raw(y, m, d):
-    """
-    Gets raw meal HTML from http://hana.hs.kr/life/meal.asp
-    """
-    date = {"yy": y, "mm": m, "dd": d}
-    r = requests.get("http://hana.hs.kr/life/meal.asp", params=date)
-    r.encoding = "utf-8"
-    return r.text
+    def update_meal(self):
+        self.y = 2019
+        self.m = 3
+        self.d = 29
+        self.meal = self.get_meal(self.y, self.m, self.d)
+        print(self.meal)
 
 
-def parse_meal(raw):
-    """
-    Parses raw meal HTML into a list
-    """
-    meals = []
-    soup = BeautifulSoup(raw, "lxml")
-    for e in soup.select_one(".today_meal").find_all("td"):
-        meals.append(e.text)
-    return meals
-
-
-def get_meal(y, m, d):
-    """
-    Uses get_raw() and parse_meal() to return meals.
-    Defaults to current year, month, and day if not specified.
-    """
-    if y == None:
-        y = now.year
-    if m == None:
-        m = now.month
-    if d == None:
-        d = now.day
-
-    raw = get_raw(y, m, d)
-    return parse_meal(raw)
+    def write_meal(self):
+        f["breakfast"] = self.meal[0]
+        f["lunch"] = self.meal[1]
+        f["dinner"] = self.meal[2]
+        f["snack"] = self.meal[3]
+        f.close()
